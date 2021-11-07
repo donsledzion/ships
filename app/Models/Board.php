438de0295c6cca73133adc8d97f144ecc7b3b11d
donsledzion <adam@ulinia8.pl>
2021-11-07 11:45:11 +0100

    Installing bootstrap layout template

---
+++ app/Models/Board.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Board extends Model
+{
+    use HasFactory;
+
+
+    protected $fillable = [
+        'user_id',
+        'table_id',
+        'fields',
+    ] ;
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+}
