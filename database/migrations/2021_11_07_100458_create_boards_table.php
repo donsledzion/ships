814829a1660acb4cad63021f063b9ef1244378ec
DESKTOP-K94QVJ7\choja <adam.chojaczyk@epicvr.pl>
2021-11-24 23:49:28 +0100

    Trying to resolve default field database exception

--- database/migrations/2021_11_07_100458_create_boards_table.php
+++ database/migrations/2021_11_07_100458_create_boards_table.php
@@ -142,7 +142,7 @@ class CreateBoardsTable extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade')->onDelete('cascade');
             $table->foreignId('table_id')->constrained()->onDelete('cascade')->onDelete('cascade');
-            $table->json('fields')->default($freshFields);
+            $table->json('fields')->default('');
             $table->boolean('initialized')->default(false);
             $table->timestamps();
         });
