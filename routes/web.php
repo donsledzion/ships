153e115476ef16c1f6ad7ffcf5b5c9752466901f
DESKTOP-K94QVJ7\choja <adam.chojaczyk@epicvr.pl>
2021-11-25 00:58:05 +0100

    Database migrated. Now resolving unknown server 500 Error

--- routes/web.php
+++ routes/web.php
@@ -30,8 +30,8 @@ Route::get('/user/{id}',[UserController::class,'show'])->name('user.show')->midd
 Route::post('/board/{board}/shot/{string}/{int}',[BoardController::class,'shot'])->middleware('auth');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/error/{string?}',function($string=""){
+/*Route::get('/error/{string?}',function($string=""){
     return view('error',[
         'message' => $string
     ]);
-});
+});*/
